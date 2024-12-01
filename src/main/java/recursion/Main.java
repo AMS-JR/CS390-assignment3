15adbce15ed01b60efa53c1a62be18a35c3778ca
Amadou Sarjo Jallow <asj.sarjo@gmail.com>
2024-12-01 17:23:47 -0600

    Init commit

--- src/main/java/recursion/Main.java
+++ src/main/java/recursion/Main.java
@@ -2,6 +2,6 @@ package recursion;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello recursion");
+        System.out.println("Hello recursion!");
     }
 }

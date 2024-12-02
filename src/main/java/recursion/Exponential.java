d09467a3bc330984c55861b2ad509a8faf967fd2
Amadou Sarjo Jallow <asj.sarjo@gmail.com>
2024-12-02 08:06:48 -0600

    Initial Commit Exponential

--- src/main/java/recursion/Exponential.java
+++ src/main/java/recursion/Exponential.java
@@ -3,5 +3,16 @@ package recursion;
 public class Exponential {
     public static void main(String[] args) {
         System.out.println("Exponential");
+        Exponential exp = new Exponential();
+        System.out.println("x^n: " + exp.power(2, 10));
+    }
+    public double power(double x, int n){
+        if(n < 0){
+            throw new IllegalArgumentException("Exponent must be non-negative");
+        }
+        if(n == 0){ // base case
+            return 1;
+        }
+        return x * power(x, n-1);
     }
 }
